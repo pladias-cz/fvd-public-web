1cc5d344130df5c1e10626e7218a6134f387a517
tomaskeb <tomaskeb@gmail.com>
2025-03-05 20:22:18 +0100

    provisional homepage

--- assets/application.js
+++ assets/application.js
@@ -1,11 +1,7 @@
-import 'bootstrap';
-import '@fortawesome/fontawesome-free/js/all'
+// import 'bootstrap';
+// import '@fortawesome/fontawesome-free/js/all'
 import './scss/index.scss';
 
-
 document.addEventListener("DOMContentLoaded", function (event) {
-
-
-
 
 });
--- htdocs/App/UI/Base/BasePresenter.php
+++ htdocs/App/UI/Base/BasePresenter.php
@@ -6,5 +6,20 @@ use Nette\Application\UI\Presenter;
 
 abstract class BasePresenter extends Presenter
 {
+    const LOCALE_CS = "cs";
+    const LOCALE_EN = "en";
+    const LOCALE_DE = "de";
+    const LOCALES = [
+        self::LOCALE_CS => self::LOCALE_CS,
+        self::LOCALE_EN => self::LOCALE_EN,
+        self::LOCALE_DE => self::LOCALE_DE];
 
+
+    const DEFAULT_DATE_FORMAT = 'j_m_Y';
+    const CS_DATE_FORMAT="j.n.Y";
+    const DE_DATE_FORMAT="j.n.Y";
+    const EN_DATE_FORMAT="j/n/Y";
+
+    /** @persistent */
+    public $locale;
 }
