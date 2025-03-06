3dae462966b190acbdc482fdb808643c4f6b8dd0
tomaskeb <tomaskeb@gmail.com>
2025-03-06 08:06:30 +0100

    nav styling

--- assets/application.js
+++ assets/application.js
@@ -1,7 +1,8 @@
 // import 'bootstrap';
 // import '@fortawesome/fontawesome-free/js/all'
 import './scss/index.scss';
+import dropdowns from './js/dropdowns.js';
 
 document.addEventListener("DOMContentLoaded", function (event) {
-
+    dropdowns();
 });
